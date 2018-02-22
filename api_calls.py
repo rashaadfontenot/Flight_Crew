d9c534f8c92a829c07807f6310d9a88b3a1c19b6
Rashaad <rashaad@rashaadfontenot.com>
2018-02-21 21:38:06 -0800

    added new variables

--- api_calls.py
+++ api_calls.py
@@ -12,12 +12,23 @@ house = urlencode({'address': address,
                    'citystatezip': citystatezip})
 
 #Formated API Link
-get_results = requests.get('http://www.zillow.com/webservice/GetSearchResults.htm?zws-id={}&{}'.format(zws_id, house))
+get_results = requests.get('http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id={}&{}'.format(zws_id, house))
 #Parse XML
 tree = ET.fromstring(get_results.content)
 
 #Get Zillow Property ID
 zpid = tree.find('response/results/result/zpid').text
 zestimate = tree.find('response/results/result/zestimate/amount').text
+bathrooms = tree.find('response/results/result/bathrooms').text
+bedrooms = tree.find('response/results/result/bedrooms').text
+lastSoldDate =  tree.find('response/results/result/lastSoldDate').text
+lastSoldPrice =  tree.find('response/results/result/lastSoldPrice').text
+yearBuilt =  tree.find('response/results/result/yearBuilt').text
+useCode =  tree.find('response/results/result/useCode').text
 
-print('The current Value for {} is ${}!'.format(address, zestimate))
+print('The current value for {} is ${}!'.format(address, zestimate))
+print('This house has {} bathrooms'.format(bathrooms))
+print('This house has {} bedrooms'.format(bedrooms))
+print('This house was last sold on {} for a price of ${}'.format(lastSoldDate, lastSoldPrice))
+print('This house was built in {}'.format(yearBuilt))
+print('This house is zoned for {}'.format(useCode))
